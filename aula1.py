9b327757fc9d2ea977252198ec526037d019a40a
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-02-07 08:18:17 -0300

    aula 2

--- aula1.py
+++ aula1.py
@@ -1,5 +1,5 @@
 """
-Não é um comentário mas o python o ingnora.
+Não é um comentário mas o python o ingnora. Usar para escrever notas ou explicar o codigo.
 """
 # Permite escrever um comentário.
 print(1+1*2)
