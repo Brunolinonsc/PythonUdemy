78a34180a52e1ebfb6f1d4e12ec409a718ebcd81
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-04-27 17:16:42 -0300

    ok

--- aula7.py
+++ aula7.py
@@ -1,1 +1,12 @@
 # Variaveis
+nome_completo = 'Bruno Lino Nascimento'
+soma_dois_mais_dois = 2+9
+print(nome_completo , soma_dois_mais_dois)
+print(soma_dois_mais_dois)
+
+nome = 'Bruno'
+idade = 28
+maior_de_idade = idade >= 18
+print('Nome:', nome)
+print('Idade:', idade)
+print('É maior?', maior_de_idade)
