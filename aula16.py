b3624f043b5552eb7f44927ed4c0d6c48f42edcf
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-06-07 21:16:22 -0300

    Novas aulas

---
+++ aula16.py
@@ -0,0 +1,12 @@
+entrada = input("Você quer entrar ou sair?")
+
+if entrada == 'entrar':
+    print('voce entrou no sistema')
+elif entrada == 'sair':
+    print('Voce saiu do sistema')
+else: 
+    print('Voce não digitou nem entrar e nem sair.')
+
+
+
+
---
+++ aula17.py
@@ -0,0 +1,11 @@
+condicao = True 
+
+if condicao: 
+    print('Esse é o codigo do if')
+else:
+    print('Else do primeiro if')
+
+if 10 == 10:
+    print('Outro if')
+    
+print('Fora do if')
